a505190fc33979616b137913de2eca2f1e969cd3
Marco Puccetti <58392926+mpuccetti@users.noreply.github.com>
2025-12-06 21:04:02 +0100

    monitor
    monitor

---
+++ monitor_client.py
@@ -0,0 +1,6 @@
+# monitor.client — simplified example
+import requests, json, time, socket
+from Crypto.Cipher import AES
+from configparser import ConfigParser
+
+# ... placeholder ...
---
+++ monitor_server.py
@@ -0,0 +1,6 @@
+# monitor.server — simplified example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Lock
+from Crypto.Cipher import AES
+import json
+# ... placeholder ...
